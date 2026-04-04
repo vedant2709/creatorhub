ed974c7f34cdbd03dbec5778aa3a82d9c8e24067
vedant-mathbuddy <vedant@mathbuddyonline.com>
2026-04-04 21:08:50 +0530

    Auth service express server setup and DB connection initialization done.

---
+++ services/auth-service/src/server.js
@@ -0,0 +1,17 @@
+import app from "./app.js"
+import { Config } from "./config/config.js"
+import connectToDB from "./config/db.js";
+
+const startServer = async () => {
+    try {
+        await connectToDB();
+        app.listen(Config.PORT, () => {
+            console.log(`Auth service running on port ${Config.PORT}`);
+        });
+    } catch (error) {
+        console.error("Failed to start server:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
