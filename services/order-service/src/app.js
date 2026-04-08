e6820a913fe2977280c4dd22f9797c9791596706
vedant-mathbuddy <vedant@mathbuddyonline.com>
2026-04-09 00:14:27 +0530

    Order service initial commit.

---
+++ services/order-service/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import errorHandler from "./middlewares/error.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+
+// app.use("/api/orders", productRoute);
+
+app.use(errorHandler)
+
+export default app;
---
+++ services/order-service/src/config/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+if(!PORT){
+    throw new Error("PORT is missing in environment variables.");
+}
+
+if(!MONGO_URI){
+    throw new Error("MONGO_URI is missing in environment variables.");
+}
+
+export const Config = {
+    PORT,
+    MONGO_URI
+}
